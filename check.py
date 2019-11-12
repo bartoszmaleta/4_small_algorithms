dfe6b2ab71e851b7fcb5191d42a7660a01477bdd
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-11-12 18:54:08 +0100

    get_odd_elements() + doctest

--- check.py
+++ check.py
@@ -28,3 +28,48 @@ def get_common_elements(first_list, second_list):
 # print(ce)
 # ce_doctest = get_common_elements([1, 2, 3], [3, 4, 5])
 # print(ce_doctest)
+# ---------------------------------------------------------------
+
+
+def get_odd_elements(x, start):
+    """
+    Function should return a list containing first 'x' odd elements,
+    starting from 'start' value.
+
+    >>> get_odd_elements(2, 31)
+    [31, 33]
+
+    >>> get_odd_elements(3, 10)
+    [11, 13, 15]
+==
+    """
+    list_witihin_range_start_and_up = []
+    for number in range(start, 100):    # chagne to while loop, because of the range!
+        list_witihin_range_start_and_up.append(number)
+    
+    list_with_odd_numbers = []
+
+    for elem in list_witihin_range_start_and_up:
+        if elem % 2 != 0:
+            list_with_odd_numbers.append(elem)
+        
+    first_x_elements_of_list_with_odd_numbers = list_with_odd_numbers[:x]
+    return first_x_elements_of_list_with_odd_numbers
+
+
+# EXERCISE 2
+# list_with_numbers = [1, 6, 3, 7, 0, 11, 13, 15]
+# # x = 1
+# # odd_numbers = get_odd_elements(x, start)
+# # print(odd_numbers)
+
+# list_with_numbers = [1, 6, 3, 7, 0, 11, 13, 15]
+# x = 3
+# start = 31
+# odd_numbers_doctest_final = get_odd_elements(2, 31)
+# print(odd_numbers_doctest_final)
+
+# list_with_numbers = [31]
+# odd_numbers_doctest_final = get_odd_elements(4, list_with_numbers)
+# print(odd_numbers_doctest_final)
+# ---------------------------------------------------------------
